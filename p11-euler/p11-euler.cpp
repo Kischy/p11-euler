43025dd81069e0852b0e5062d72cde58d789b57f
Christian Häge <christian.haege@gmx.net>
2019-03-21 18:01:43 +0100

    Added VS project and VS gtest project

---
+++ p11-euler/p11-euler.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+
+
+int main(int argc, char **argv)
+{
+	unsigned long long p11_answ = 0;
+
+
+
+	std::cout << "The answer to the 11th problem of ProjectEuler.Net is " << p11_answ << ".\n";
+
+
+	return 0;
+}

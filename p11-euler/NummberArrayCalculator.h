8d63e07236dbdf1002da6a31d5d568de21d85b7e
chaege_Thinkpad <c.haege@ipc.uni-stuttgart.de>
2019-03-29 16:53:33 +0100

    Train is running into Kassel

--- p11-euler/NummberArrayCalculator.h
+++ p11-euler/NummberArrayCalculator.h
@@ -3,12 +3,70 @@
 #include <vector>
 #include <string>
 
+#include <fstream>
+
+
+bool is_number(const std::string& s)
+{
+	uint i = 0;
+	if (s.size() > 0)
+	{
+		if (s[0] == '+' || s[0] == '-')
+			i++;
+	}
+	else
+		return false;
+
+	int NoOfPoints = 0;
+
+	for (; i < s.size(); i++)
+	{
+		if (std::ispunct(s[i]))
+		{
+			if (s[i] == '.')
+				NoOfPoints++;
+			else
+				return false;
+		}
+		else if (!std::isdigit(s[i]))
+			return false;
+	}
+
+	if (NoOfPoints > 1)
+		return false;
+
+	return true;
+}
+
+
+
+template<typename T>
+T str_to_number(std::string s)
+{
+	std::stringstream ss(s);
+	T res;
+	ss >> res;
+	return res;
+
+}
+
+
+
+
 class NummberArrayCalculator
 {
 public:
-	std::vector< unsigned int > convert_to_vector(const std::string number_as_string) const
+	std::vector< unsigned int > convert_to_vector(const std::string numbers_as_string) const
 	{
-		std::vector<unsigned int> the_numbers{};
+		bool is_number = is_number(numbers_as_string);
+		unsigned int number = 0;
+		
+		if (is_number)
+		{
+			number = str_to_number<unsigned int>(numbers_as_string);
+		}
+
+		std::vector<unsigned int> the_numbers{number};
 
 		return the_numbers;
 	}
